4f99d6c6d381a91f4c582d49fee78b6141cea75d
lqr131115 <1046975211@qq.com>
2023-04-25 14:44:40 +0800

    feat: search 分页

--- packages/install/lib/install.js
+++ packages/install/lib/install.js
@@ -1,11 +1,14 @@
 import Command from '@e.ucli/command';
-import { log, makeList, Github, Gitee, getPlatform, removeGitCacheFile, createGitCacheFile, makeInput } from '@e.ucli/utils';
+import { log, makeList, Github, Gitee, getPlatform, removeGitCacheFile, createGitCacheFile, makeInput, printErrorLog } from '@e.ucli/utils';
 
 const PLAT_GITHUB_VAL = 'github'
 const PLAT_GITEE_VAL = 'gitee'
 
 const SEARCH_MODE_PROP = 'repositories'
 const SEARCH_MODE_CODE = 'code'
+
+const GITEE_LANGUAGES = ['Java', 'JavaScript', 'TypeScript', 'HTML', 'CSS']
+const GITHUB_LANGUAGES = ['java', 'javascript', 'typeScript', 'html', 'css']
 
 class InstallCommand extends Command {
   constructor(instance) {
@@ -32,7 +35,7 @@ class InstallCommand extends Command {
       this.resetGitApi()
     }
     await this.initGitApi(opts)
-    await this.doSearch(opts)
+    await this.searchGitApi(opts)
   }
 
   async initGitApi(opts) {
@@ -65,42 +68,86 @@ class InstallCommand extends Command {
     this.gitApi = gitApi
     this.mode = mode
     this.platform = platform
+
+    this.searchPage = opts.page ?? 1
+    this.searchPagesize = opts.pagesize ?? 5
   }
 
-  async doSearch(opts) {
+  async searchGitApi(opts) {
+    const { q, language } = await this.getInputQuery()
+    const params = { ...opts, q, language }
+    await this.doSearch(params)
+  }
+
+  async doSearch(params) {
     let list;
-    let q = await makeInput({
-      message: '请输入搜索关键字',
-      defaultValue: '',
-      validate(v) {
-        if (v && v.length > 0) {
-          return true
-        }
-        return '项目名称不能为空'
-      }
-    })
     if (this.mode === SEARCH_MODE_PROP) {
-      list = await this.doSearchRepositories({ ...opts, q })
+      list = await this.doSearchRepositories(params)
     }
     else if (this.mode === SEARCH_MODE_CODE) {
-      list = await this.doSearchCode({ ...opts, q })
+      list = await this.doSearchCode(params)
     }
     else {
       list = []
     }
-    let repository = await makeList({
+    if (list.length === 0) {
+      log.info('没有搜索到内容')
+      return
+    }
+    if (this.searchPage > 1) {
+      list.push({ name: '上一页', value: 'prev' })
+    }
+    if (this.searchPage <= 10) {
+      list.push({ name: '下一页', value: 'next' })
+    }
+    let keyword = await makeList({
       choices: list
     })
+    if (keyword === 'next') {
+      await this.nextSearchPage(params)
+    }
+    else if (keyword === 'prev') {
+      await this.prevSearchPage(params)
+    }
+  }
+
+  async getInputQuery() {
+    let q, language, choices = [];
+    if (this.platform === PLAT_GITEE_VAL) {
+      choices = GITEE_LANGUAGES.map((lan) => ({ name: lan, value: lan }))
+    }
+    else if (this.platform === PLAT_GITHUB_VAL) {
+      choices = GITHUB_LANGUAGES.map((lan) => ({ name: lan, value: lan }))
+    }
+    try {
+      q = await makeInput({
+        message: '请输入搜索关键字',
+        validate(v) {
+          if (v && v.length > 0) {
+            return true
+          }
+          return '项目名称不能为空'
+        }
+      })
+      language = await makeList({
+        choices,
+        message: '请选择开发语言',
+      })
+    } catch (error) {
+      printErrorLog(error)
+    }
+    return { q, language }
   }
 
   async doSearchRepositories(params) {
-    const { sort, pagesize, page } = params
+    const { sort, pagesize, page, q, language } = params
     if (this.platform === PLAT_GITEE_VAL) {
       params = { ...params, sort: `${sort}_count`, per_page: pagesize }
     }
     else if (this.platform === PLAT_GITHUB_VAL) {
-      params = { ...params, per_page: pagesize, page: +page }
+      params = { ...params, per_page: pagesize, page: page, q: `${q}+language:${language}` }
     }
+    console.log('doSearchRepositories params', params)
     let searchResult = await this.gitApi.searchRepositories(params)
     if (this.platform === PLAT_GITHUB_VAL) {
       searchResult = searchResult.items
@@ -114,21 +161,31 @@ class InstallCommand extends Command {
   }
 
   async doSearchCode(params) {
-    const { pagesize } = params
+    const { pagesize, q, language } = params
     if (this.platform === PLAT_GITHUB_VAL) {
-      params = { ...params, per_page: pagesize }
+      params = { ...params, per_page: pagesize, q: `${q}+language:${language}` }
     }
     let searchResult = await this.gitApi.searchCode(params)
     if (this.platform === PLAT_GITHUB_VAL) {
       searchResult = searchResult.items
     }
+
     return searchResult.map(item => {
       return {
         name: `${item.repository.full_name}(${item.repository.description})`,
         value: item.repository.id
       }
     })
-    console.log(res)
+  }
+
+  async nextSearchPage(params) {
+    this.searchPage += 1
+    await this.doSearch({ ...params, page: this.searchPage })
+  }
+
+  async prevSearchPage(params) {
+    this.searchPage -= 1
+    await this.doSearch({ ...params, page: this.searchPage })
   }
 
   resetGitApi() {
